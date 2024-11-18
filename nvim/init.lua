e772c0b49cd7522404261f911c47a1c60bef0a1d
mo <moe.rifaii77@gmail.com>
2024-11-18 08:13:11 +0200

    move lazy.nvim to a separate file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,68 +1,28 @@
 vim.api.nvim_set_option_value("clipboard", "unnamedplus", {})
-
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
-
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-
--- Auto-install lazy.nvim if not present
-if not vim.uv.fs_stat(lazypath) then
-  print('Installing lazy.nvim....')
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-  print('Done.')
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup({
-    {'folke/tokyonight.nvim'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'windwp/nvim-autopairs'},
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
-    },
-    {'windwp/nvim-ts-autotag'},
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}, --TODO: configure parsers installation here
-})
-
 vim.opt.termguicolors = true
-vim.cmd.colorscheme('tokyonight')
-
--- Reserve a space in the gutter
--- This will avoid an annoying layout shift in the screen
 vim.opt.signcolumn = 'yes'
+
+require('pm') --loads lazy.nvim
+
+vim.cmd.colorscheme('tokyonight')
 
 require('nvim-autopairs').setup({})
 require('nvim-ts-autotag').setup({
   opts = {
-    -- Defaults
     enable_close = true, -- Auto close tags
     enable_rename = true, -- Auto rename pairs of tags
     enable_close_on_slash = false -- Auto close on trailing </
   },
-  -- Also override individual filetype configs, these take priority.
-  -- Empty by default, useful if one of the "opts" global settings
-  -- doesn't work well in a specific filetype
   per_filetype = {
     ["html"] = {
       enable_close = true
     }
   }
 })
-require('lsp')
 
+require('lsp')
