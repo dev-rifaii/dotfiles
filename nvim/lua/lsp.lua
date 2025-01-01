ebc318be93d4a3af40b9d88760f225cbfb50f67f
mo <moe.rifaii77@gmail.com>
2025-01-01 19:35:35 +0200

    disable unused functions diagnostics for lua LSP

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -2,57 +2,65 @@
 -- This should be executed before you configure any language server
 local lspconfig_defaults = require('lspconfig').util.default_config
 lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lspconfig_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
+    'force',
+    lspconfig_defaults.capabilities,
+    require('cmp_nvim_lsp').default_capabilities()
 )
 
 -- This is where you enable features that only work
 -- if there is a language server active in the file
 vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    local opts = {buffer = event.buf}
+    desc = 'LSP actions',
+    callback = function(event)
+        local opts = { buffer = event.buf }
 
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-  end,
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    end,
 })
 
 --LSP
-require('lspconfig').lua_ls.setup({})
+require('lspconfig').lua_ls.setup({
+    settings = {
+        Lua = {
+            diagnostics = {
+                disable = { 'unused-function' },
+            }
+        }
+    }
+})
 require('lspconfig').jdtls.setup({
-  root_dir = require('lspconfig').util.root_pattern('pom.xml', 'build.gradle', '.git'),
-  settings = {
-      java = {
-          -- Custom settings
-      }
-  }
+    root_dir = require('lspconfig').util.root_pattern('pom.xml', 'build.gradle', '.git'),
+    settings = {
+        java = {
+            -- Custom settings
+        }
+    }
 })
 require('lspconfig').ts_ls.setup({})
 
 --Auto complete
 local cmp = require('cmp')
 cmp.setup({
-    completion = { completeopt = 'menu,menuone,noinsert'},
+    completion = { completeopt = 'menu,menuone,noinsert' },
     sources = {
-        {name = 'nvim_lsp'},
+        { name = 'nvim_lsp' },
     },
     mapping = cmp.mapping.preset.insert({
         -- Navigate between completion items
-        ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-        ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
 
         -- `Enter` key to confirm completion
-        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
 
         -- Ctrl+Space to trigger completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
@@ -63,9 +71,9 @@ cmp.setup({
     }),
     snippet = {
         expand = function(args)
-        vim.snippet.expand(args.body)
-    end,
-  },
+            vim.snippet.expand(args.body)
+        end,
+    },
 })
 
 --vim.lsp.set_log_level("debug")
