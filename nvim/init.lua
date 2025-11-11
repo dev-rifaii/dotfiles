a19f07b0c50979fb2f791996c83fd79e22a9f8d9
mo <moe.rifaii77@gmail.com>
2025-11-12 01:27:02 +0200

    new

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_set_option_value("clipboard", "unnamedplus", {})
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4      -- A TAB character looks like 4 spaces
+vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.opt.softtabstop = 4  -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 4   -- Number of spaces inserted when indenting
+vim.opt.termguicolors = true
+vim.opt.signcolumn = 'yes'
+vim.wo.wrap = false
+
